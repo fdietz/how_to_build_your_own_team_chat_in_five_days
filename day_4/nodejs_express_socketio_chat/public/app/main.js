235abe59511ce6b26aa5292de109675680c2e498
Frederik Dietz <fdietz@gmail.com>
2015-04-16 16:52:44 +0200

    add avatar directive

--- day_4/nodejs_express_socketio_chat/public/app/main.js
+++ day_4/nodejs_express_socketio_chat/public/app/main.js
@@ -6,12 +6,14 @@ import MessageListModule from './message_list/message_list.module';
 import MessageFormModule from './message_form/message_form.module';
 import ParticipantsListModule from './participants_list/participants_list.module';
 import SidebarModule from './sidebar/sidebar.module';
+import AvatarModule from './avatar/avatar.module';
 
 let mainModule = angular.module('mainApp', [
     MessageListModule.name,
     MessageFormModule.name,
     ParticipantsListModule.name,
-    SidebarModule.name
+    SidebarModule.name,
+    AvatarModule.name
   ]).service(WebSocket.name, WebSocket);
 
 export default mainModule;

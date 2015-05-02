46d4b694bd48a486f76a008bd92e8ee0b9d25653
Frederik Dietz <fdietz@gmail.com>
2015-05-02 12:40:59 +0200

    fix jspm dependencies

--- day_4/nodejs_express_socketio_chat/public/config.js
+++ day_4/nodejs_express_socketio_chat/public/config.js
@@ -21,12 +21,13 @@ System.config({
     "core-js": "npm:core-js@0.8.3",
     "jquery": "github:components/jquery@2.1.3",
     "text": "github:systemjs/plugin-text@0.0.2",
+    "underscore": "npm:underscore@1.8.3",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:core-js@0.8.3": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     }
-  },
+  }
 });
 
